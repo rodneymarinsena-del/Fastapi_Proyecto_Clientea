7ab192b11e86a4f02e3d93a3c98ab0e0d54f36c2
Rodney Marin <rodneymarinsena@gmail.com>
2026-06-24 11:34:12 -0500

    se hiciron la clase cliente tambien lista cliente cliente id y crear cliente

---
+++ README.md
@@ -0,0 +1,3 @@
+este es el tercer comit 
+ya esoy conasado asi que mejor con tinuo mañana 
+se hiciron la calse cliente tambien lista cliente cliente id y crear clien
--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-def inicio():
-    return {"mensaje": "hola estoy aprendiendo FASAPI"}
+# Modelo del Cliente
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
+
+# Base de datos en memoria
+lista_clientes: List[Cliente] = []
+
+# Endpoint: Listar todos
+@app.get("/clientes")
+def listar_clientes():
+    return lista_clientes
+
+# Endpoint: Listar uno solo
+@app.get("/clientes/{cliente_id}")
+def listar_cliente(cliente_id: int):
+    for cliente in lista_clientes:
+        if cliente.id == cliente_id:
+            return cliente
+    return {"mensaje": "Cliente no encontrado"}
+
+# Endpoint: Crear cliente
+@app.post("/clientes")
+def crear_cliente(cliente: Cliente):
+    lista_clientes.append(cliente)
+    return cliente
