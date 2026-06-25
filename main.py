4838d77a6b284cd7ac669ef1388d1f68437eb2d2
Rodney Marin <rodneymarinsena@gmail.com>
2026-06-25 10:41:29 -0500

    editó el end point listar un sol una sola factura y el manejo de excepciones

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import List
 from modelos.clientes import Cliente, ClienteCrear, ClienteEditar
@@ -7,12 +7,12 @@ from modelos.transacciones import Transaccion, TransaccionCrear, TransaccionEdit
 
 app = FastAPI()
 
-# Base de datos en memoria para cada modelo
+# Base de datos en memoria
 lista_clientes: List[Cliente] = []
 lista_facturas: List[Factura] = []
 lista_transacciones: List[Transaccion] = []
 
-# ENDPOINTS CLIENTES 
+# --- ENDPOINTS CLIENTES ---
 @app.get("/clientes", response_model=List[Cliente])
 async def listar_clientes():
     return lista_clientes
@@ -22,7 +22,7 @@ async def listar_cliente(cliente_id: int):
     for cliente in lista_clientes:
         if cliente.id == cliente_id:
             return cliente
-    raise HTTPException(status_code=400, detail=f"El cliente con ID {cliente_id} no existe")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"El cliente con ID {cliente_id} no existe")
 
 @app.post("/clientes", response_model=Cliente)
 async def crear_cliente(cliente_crear: ClienteCrear):
@@ -40,7 +40,7 @@ async def editar_cliente(cliente_id: int, datos_cliente: ClienteEditar):
             val.id = cliente_id
             lista_clientes[i] = val
             return val
-    raise HTTPException(status_code=400, detail=f"El cliente con ID {cliente_id} no existe")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"El cliente con ID {cliente_id} no existe")
 
 @app.delete("/clientes/{cliente_id}", response_model=Cliente)
 async def eliminar_cliente(cliente_id: int):
@@ -48,20 +48,44 @@ async def eliminar_cliente(cliente_id: int):
         if objeto_cliente.id == cliente_id:
             cliente_eliminado = lista_clientes.pop(i)
             return cliente_eliminado
-    raise HTTPException(status_code=400, detail=f"El cliente con ID {cliente_id} no existe")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"El cliente con ID {cliente_id} no existe")
 
-# ENDPOINTS FACTURAS 
+# --- ENDPOINTS FACTURAS ---
 @app.get("/facturas", response_model=List[Factura])
 async def listar_facturas():
     return lista_facturas
 
 @app.get("/facturas/{factura_id}", response_model=Factura)
 async def listar_factura(factura_id: int):
-    pass
+    for factura in lista_facturas:
+        if factura.id == factura_id:
+            return factura
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"La factura con ID {factura_id} no existe")
 
 @app.post("/facturas/{cliente_id}", response_model=Factura)
 async def crear_factura(cliente_id: int, factura_crear: FacturaCrear):
-    pass
+
+    cliente_encontrado = None
+    for cliente in lista_clientes:
+        if cliente.id == cliente_id:
+            cliente_encontrado = cliente
+            break
+    
+    if not cliente_encontrado:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail=f"El cliente con ID {cliente_id} no existe, no se puede crear la factura"
+        )
+    
+    id_factura = len(lista_facturas) + 1
+    val = Factura.model_validate(factura_crear.model_dump())
+    val.id = id_factura
+    
+    val.cliente = cliente_encontrado
+    
+   
+    lista_facturas.append(val)
+    return val
 
 @app.patch("/facturas/{factura_id}", response_model=Factura)
 async def editar_factura(factura_id: int, datos_factura: FacturaEditar):
@@ -71,7 +95,7 @@ async def editar_factura(factura_id: int, datos_factura: FacturaEditar):
 async def eliminar_factura(factura_id: int):
     pass
 
-# ENDPOINTS TRANSACCIONES 
+# --- ENDPOINTS TRANSACCIONES ---
 @app.get("/transacciones", response_model=List[Transaccion])
 async def listar_transacciones():
     return lista_transacciones
